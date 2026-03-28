54c94b85768009d82e275150e1df9f3a759b0ce8
kp7227-srm <kp7227@srmist.edu.in>
2026-03-28 23:41:34 +0530

    uc2

---
+++ uc2.java
@@ -0,0 +1,8 @@
+public class uc2 {
+    public static void main(String[] args) {
+        if (args.length > 0 && args[0] != null && !args[0].isEmpty()) {
+            String name = args[0];
+            System.out.println("Hello " + name);
+        }
+    }
+}
