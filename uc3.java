cef72e74c598a98af73c22110005739c19af19e1
kp7227-srm <kp7227@srmist.edu.in>
2026-03-28 23:54:25 +0530

    uc3

---
+++ uc3.java
@@ -0,0 +1,14 @@
+/* 
+ * @author Developer Name
+ * @version 3.0
+ * @since UC3
+ */
+public class uc3 {
+    public static void main(String[] args) {
+        String name = "World";
+        if (args.length > 0 && args[0] != null && !args[0].isEmpty()) {
+            name = args[0];
+        }
+        System.out.println("Hello " + name + "!");
+    }
+}
