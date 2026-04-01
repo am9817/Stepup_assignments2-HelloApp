1a055193d3213f23ff106a324a2ef6929f2d41bd
am9817 <am9817@srmist.edu.in>
2026-04-02 03:42:40 +0530

    Update uc2.java

--- uc2.java
+++ uc2.java
@@ -1,1 +1,9 @@
+public class uc2 {
+    public static void main(String[] args) {
+        if (args.length > 0 && args[0] != null && !args[0].isEmpty()) {
+            String name = args[0];
+            System.out.println("Hello " + name);
+        }
+    }
+}
 
