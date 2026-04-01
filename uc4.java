99549fd86a4103f47504326eca26c1573e23d4d4
am9817 <am9817@srmist.edu.in>
2026-04-02 03:38:04 +0530

    Update uc4.java

--- uc4.java
+++ uc4.java
@@ -1,1 +1,17 @@
 
+public class uc4 {
+    public static void main(String[] args) {
+        // Check if the array is empty
+        if (args.length > 0) {
+            // Iterate through each argument provided
+            for (String name : args) {
+                if (name != null && !name.isEmpty()) {
+                    System.out.println("Hello " + name + "!");
+                }
+            }
+        } else {
+            // Default case if no arguments are passed
+            System.out.println("Hello World!");
+        }
+    }
+}
